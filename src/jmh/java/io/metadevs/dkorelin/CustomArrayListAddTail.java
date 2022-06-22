dd757452ed13912ab77a16eecb930d311e41bfe4
DKorelin <dkorelin@meta-devs.io>
2022-06-22 23:13:23 +0500

    CustomArrayList

---
+++ src/jmh/java/io/metadevs/dkorelin/CustomArrayListAddTail.java
@@ -0,0 +1,33 @@
+package io.metadevs.dkorelin;
+
+import io.metadevs.dkorelin.CustomArrayList.CustomArrayList;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class CustomArrayListAddTail {
+    @Benchmark
+    public void addBenchmark100() {
+        addElements(100);
+    }
+
+    @Benchmark
+    public void addBenchmark1000() {
+        addElements(1000);
+    }
+
+    @Benchmark
+    public void addBenchmark10000() {
+        addElements(10000);
+    }
+
+    public void addElements(int numOfElements) {
+        CustomArrayList<String> caString = new CustomArrayList<>(numOfElements);
+        for (int i = 0; i < numOfElements; i++) {
+            caString.add("test"+i);
+        }
+    }
+}
+
+
